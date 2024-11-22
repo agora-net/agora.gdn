c2a6c082cdc66c667a43fc2aac24a082d9351488
Ewan Jones <egcjones@gmail.com>
2024-11-22 16:50:40 +0100

    add configuration directory with systemd files for gunicorn

---
+++ conf/gunicorn/gunicorn.conf.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+accesslog = "-"
+bind = "unix:/run/gunicorn.sock"
+workers = multiprocessing.cpu_count() * 2 + 1
+max_requests = 1_000
+max_requests_jitter = 100
+timeout = 5
+graceful_timeout = 5
---
+++ conf/systemd/README.md
@@ -0,0 +1,7 @@
+# Systemd
+
+Use `systemd` to manage starting the service and keeping it running.
+
+## Useful links
+
+- https://codingnomads.com/django-gunicorn-service
