3c5501854b6e5fbc23ad1158327c660646478ac3
Ewan Jones <egcjones@gmail.com>
2024-11-27 21:51:02 +0100

    update gunicorn config to bind to PORT env var

--- conf/gunicorn/gunicorn.conf.py
+++ conf/gunicorn/gunicorn.conf.py
@@ -1,7 +1,8 @@
 import multiprocessing
+import os
 
 accesslog = "-"
-bind = ["0.0.0.0:8000", "unix:/run/gunicorn/gunicorn.sock"]
+bind = ["unix:/run/gunicorn/gunicorn.sock", f"0.0.0.0:{os.getenv('PORT', 8000)}"]
 workers = multiprocessing.cpu_count() * 2 + 1
 max_requests = 1_000
 max_requests_jitter = 100
