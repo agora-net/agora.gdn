c34d79aba468ef36c4527bab4e6708d5da868358
Ewan Jones <egcjones@gmail.com>
2024-12-02 21:22:56 +0100

    add careers page

--- agora/urls.py
+++ agora/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
+    path("careers/", TemplateView.as_view(template_name="home/careers.html"), name="careers"),
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
