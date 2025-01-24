09eaa3605c155be05415cd22f476207fb4a2bdbf
Ewan Jones <egcjones@gmail.com>
2025-01-24 14:19:25 +0100

    add Snowflake ID primary key mixin to models

---
+++ utils/models.py
@@ -0,0 +1,19 @@
+import os
+
+from django.db import models
+from snowflake import SnowflakeGenerator
+
+SNOWFLAKE_GENERATOR = SnowflakeGenerator(instance=(os.getpid() % 1024))
+
+
+def snowflake_generator():
+    return next(SNOWFLAKE_GENERATOR)
+
+
+class SnowflakeIdPrimaryKeyMixin(models.Model):
+    id = models.PositiveBigIntegerField(
+        primary_key=True, default=snowflake_generator, editable=False
+    )
+
+    class Meta:
+        abstract = True
