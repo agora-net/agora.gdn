7d679c3c69585cc065d7dda7b7c0145d74f6484f
Ewan Jones <egcjones@gmail.com>
2025-01-25 16:42:05 +0100

    add type hints to onboarding_not_required decorator for improved type safety

--- user/decorators.py
+++ user/decorators.py
@@ -1,4 +1,16 @@
-def onboarding_not_required(view_func):
+from typing import Any, Protocol, TypeVar
+
+
+class ViewFunction(Protocol):
+    onboarding_required: bool
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any: ...
+
+
+F = TypeVar("F", bound=ViewFunction)
+
+
+def onboarding_not_required(view_func: F) -> F:
     """
     Decorator for views that removes the need for a user to be fully onboarded.
     """
