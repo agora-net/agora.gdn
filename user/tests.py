04ec027305547221c560292fc02e7deacfb4a57f
Ewan Jones <egcjones@gmail.com>
2025-01-26 15:45:57 +0100

    add tests for FullyOnboardedUserRequiredMiddleware to verify onboarding redirection

--- user/tests.py
+++ user/tests.py
@@ -1,1 +1,16 @@
-# Create your tests here.
+from django.test import TestCase
+
+
+class FullyOnboardedUserRequiredMiddlewareTestCase(TestCase):
+    def test_onboarding_middleware(self):
+        with self.settings(
+            MIDDLEWARE=[
+                "django.contrib.sessions.middleware.SessionMiddleware",
+                "django.contrib.auth.middleware.AuthenticationMiddleware",
+                "allauth.account.middleware.AccountMiddleware",
+                "user.middleware.FullyOnboardedUserRequiredMiddleware",
+            ]
+        ):
+            response = self.client.get("/")
+
+            self.assertRedirects(response, "/accounts/login/")
