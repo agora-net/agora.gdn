3f5b866f15e1796e0ad4b70bf94eae8b0ad1847f
Ewan Jones <egcjones@gmail.com>
2025-01-27 09:54:11 +0100

    fix tests

--- user/tests.py
+++ user/tests.py
@@ -3,14 +3,5 @@ from django.test import TestCase
 
 class FullyOnboardedUserRequiredMiddlewareTestCase(TestCase):
     def test_onboarding_middleware(self):
-        with self.settings(
-            MIDDLEWARE=[
-                "django.contrib.sessions.middleware.SessionMiddleware",
-                "django.contrib.auth.middleware.AuthenticationMiddleware",
-                "allauth.account.middleware.AccountMiddleware",
-                "user.middleware.FullyOnboardedUserRequiredMiddleware",
-            ]
-        ):
-            response = self.client.get("/")
-
-            self.assertRedirects(response, "/accounts/login/")
+        response = self.client.get("/")
+        self.assertRedirects(response, "/accounts/login/")
