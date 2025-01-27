e4d19dfd2faf5a9160d9a642a259e9938911fb2d
Ewan Jones <egcjones@gmail.com>
2025-01-27 09:57:31 +0100

    update tests

--- user/tests.py
+++ user/tests.py
@@ -2,6 +2,13 @@ from django.test import TestCase
 
 
 class FullyOnboardedUserRequiredMiddlewareTestCase(TestCase):
+    def test_without_onboarding_middleware_works_as_expected(self):
+        with self.modify_settings(
+            MIDDLEWARE={"remove": "user.middleware.FullyOnboardedUserRequiredMiddleware"}
+        ):
+            response = self.client.get("/")
+            self.assertEqual(response.status_code, 200)
+
     def test_onboarding_middleware(self):
         response = self.client.get("/")
         self.assertRedirects(response, "/accounts/login/")
