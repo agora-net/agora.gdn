c4dc9f9f4fd1a3d12a9758bbda72353e8b21ed72
Ewan Jones <egcjones@gmail.com>
2025-01-27 09:39:01 +0100

    properly type password validator

--- user/password_validation.py
+++ user/password_validation.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
 
 
 class MaximumLengthValidator:
-    def __init__(self, max_length=64):
+    def __init__(self, max_length: int = 64):
         self.max_length = max_length
 
-    def validate(self, password, user=None):
+    def validate(self, password: str, user: AbstractUser | None = None) -> None:
         if len(password) > self.max_length:
             raise ValidationError(
                 _("This password must contain no more than %(max_length)d characters."),
@@ -14,5 +15,5 @@ class MaximumLengthValidator:
                 params={"max_length": self.max_length},
             )
 
-    def get_help_text(self):
+    def get_help_text(self) -> str:
         return _(f"Your password must be shorter than {self.max_length} characters.")
