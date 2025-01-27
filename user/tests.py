ae86e9aee15ff3e5d9ae9766774552e84c617f9d
Ewan Jones <egcjones@gmail.com>
2025-01-27 11:48:12 +0100

    fix typechecking in tests

--- user/tests.py
+++ user/tests.py
@@ -2,13 +2,13 @@ from django.test import TestCase
 
 
 class FullyOnboardedUserRequiredMiddlewareTestCase(TestCase):
-    def test_without_onboarding_middleware_works_as_expected(self):
+    def test_without_onboarding_middleware_works_as_expected(self) -> None:
         with self.modify_settings(
             MIDDLEWARE={"remove": "user.middleware.FullyOnboardedUserRequiredMiddleware"}
         ):
             response = self.client.get("/")
             self.assertEqual(response.status_code, 200)
 
-    def test_onboarding_middleware(self):
+    def test_onboarding_middleware(self) -> None:
         response = self.client.get("/")
         self.assertRedirects(response, "/accounts/login/")
