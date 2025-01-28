ea5821c6fe07d18a56cc144c2ff6d2db403b4a30
Ewan Jones <egcjones@gmail.com>
2025-01-28 12:08:06 +0100

    set up Playwright for e2e tests and manage browser lifecycle

--- user/tests/test_e2e.py
+++ user/tests/test_e2e.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import tag
 from playwright.sync_api import Browser, Playwright, sync_playwright
@@ -16,9 +18,21 @@ class UserRegistrationTestCase(StaticLiveServerTestCase):
         """
         return self.live_server_url
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+        super().setUpClass()
+        cls.playwright = sync_playwright().start()
+        cls.browser = cls.playwright.chromium.launch()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+        os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = ""
+        cls.browser.close()
+        cls.playwright.stop()
+
     def test_user_registration(self) -> None:
-        with sync_playwright() as p:
-            browser = p.firefox.launch()
-            page = browser.new_page()
-            page.goto(self.get_server_url())
-            self.assertEqual(page.title(), "Sign In")
+        page = self.browser.new_page()
+        page.goto(self.get_server_url())
+        self.assertEqual(page.title(), "Sign In")
