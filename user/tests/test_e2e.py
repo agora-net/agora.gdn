bd8dfc38c7e24333918cc72a808dfcc0e8b614ff
Ewan Jones <egcjones@gmail.com>
2025-01-28 12:30:29 +0100

    expand e2e tests to test signup

--- user/tests/test_e2e.py
+++ user/tests/test_e2e.py
@@ -2,7 +2,10 @@ import os
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import tag
-from playwright.sync_api import Browser, Playwright, sync_playwright
+from django.urls import reverse
+from playwright.sync_api import Browser, Playwright, expect, sync_playwright
+
+from .utils import faker
 
 
 @tag("e2e")
@@ -17,6 +20,9 @@ class UserRegistrationTestCase(StaticLiveServerTestCase):
         so we can run the tests against a different (i.e. live) server.
         """
         return self.live_server_url
+
+    def get_route(self, route_name: str) -> str:
+        return self.get_server_url() + reverse(route_name)
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -33,6 +39,33 @@ class UserRegistrationTestCase(StaticLiveServerTestCase):
         cls.playwright.stop()
 
     def test_user_registration(self) -> None:
+        """
+        Jamie is a new user who wants to sign up for the service.
+        """
+        user_email = faker.fake_email()
         page = self.browser.new_page()
-        page.goto(self.get_server_url())
+
+        # First of all they visit the login page
+        page.goto(self.get_route("account_login"))
         self.assertEqual(page.title(), "Sign In")
+
+        # They see a link to sign up and click it
+        page.click("a:has-text('Sign up')")
+        page.wait_for_url(self.get_route("account_signup"))
+        self.assertEqual(page.title(), "Signup")
+
+        # They enter their email address and password and submit the form
+        insecure_password = "password"
+        page.fill("input[name=email]", user_email)
+        page.fill("input[name=password1]", insecure_password)
+        page.fill("input[name=password2]", insecure_password)
+        page.click("button:has-text('Sign up')")
+
+        # Unfortunately the password is too insecure and they are redirected back to the signup page
+        # with some validation errors
+        page.wait_for_url(self.get_route("account_signup"))
+        self.assertEqual(page.title(), "Signup")
+        errorlist = page.locator("ul.errorlist")
+        self.assertTrue(errorlist.is_visible())
+        expect(errorlist).to_contain_text("This password is too common.")
+        expect(errorlist).to_contain_text("This password is too short.")
