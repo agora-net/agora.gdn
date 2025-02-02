10dab9e88c0abb9f2065ed9302642d120a4d4c51
Ewan Jones <egcjones@gmail.com>
2025-02-02 21:23:45 +0100

    move to class based views for now

--- user/views.py
+++ user/views.py
@@ -1,31 +1,43 @@
+from typing import Any
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 
 from . import selectors
 from .decorators import onboarding_not_required
 
 
-class OnboardingNotRequiredMixin:
-    """Means that the user does not need to be fully onboarded to access the view."""
+@method_decorator(onboarding_not_required, name="dispatch")
+class OnboardingBillingView(TemplateView):
+    template_name = "user/onboarding/billing.html"
+    http_method_names = ["get"]
 
-    onboarding_required = False
-
-
-@onboarding_not_required
-def onboarding_billing(request: HttpRequest) -> HttpResponse:
-    redirect_route = selectors.next_onboarding_step_route(user=request.user)
-    if redirect_route is None:
-        return redirect("profile")
-    if redirect_route != "onboarding_billing":
-        return redirect(redirect_route)
-    return HttpResponse()
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        redirect_route = selectors.next_onboarding_step_route(user=request.user)
+        if redirect_route is None:
+            return redirect("profile")
+        if redirect_route != "onboarding_billing":
+            return redirect(redirect_route)
+        return super().get(request, *args, **kwargs)
 
 
-@onboarding_not_required
-def onboarding_identity(request: HttpRequest) -> HttpResponse:
-    redirect_route = selectors.next_onboarding_step_route(user=request.user)
-    if redirect_route is None:
-        return redirect("profile")
-    if redirect_route != "onboarding_identity":
-        return redirect(redirect_route)
-    return HttpResponse()
+onboarding_billing = OnboardingBillingView.as_view()
+
+
+@method_decorator(onboarding_not_required, name="dispatch")
+class OnboardingIdentityView(TemplateView):
+    template_name = "user/onboarding/identity.html"
+    http_method_names = ["get"]
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        redirect_route = selectors.next_onboarding_step_route(user=request.user)
+        if redirect_route is None:
+            return redirect("profile")
+        if redirect_route != "onboarding_identity":
+            return redirect(redirect_route)
+        return super().get(request, *args, **kwargs)
+
+
+onboarding_identity = OnboardingIdentityView.as_view()
