108be4bc16825dd4538a82938bbd8e96467372b3
Ewan Jones <egcjones@gmail.com>
2025-02-14 22:42:44 +0100

    add md5 filter to compute MD5 hash of a value

--- user/templatetags/user.py
+++ user/templatetags/user.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from django import template
 
 register = template.Library()
@@ -6,3 +8,8 @@ register = template.Library()
 @register.filter
 def get_form_field(form, field_name):
     return form[field_name]
+
+
+@register.filter
+def md5(value):
+    return hashlib.md5(str(value).encode()).hexdigest()
