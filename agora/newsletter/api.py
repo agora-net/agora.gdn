b1789c0a6a038dcb223147e5c0d0e3e1baafa65e
Ewan Jones <ewan@climacrux.com>
2025-06-27 12:00:04 +0200

    feat: enhance newsletter signup functionality by setting a cookie and updating form visibility logic

--- agora/newsletter/api.py
+++ agora/newsletter/api.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from ninja import Router
 from pydantic import BaseModel
 
@@ -13,6 +13,11 @@ class NewsletterSignupSchema(BaseModel):
 
 
 @router.post("/signup")
-def newsletter_signup_api(request: HttpRequest, payload: NewsletterSignupSchema):
+def newsletter_signup_api(
+    request: HttpRequest,
+    response: HttpResponse,
+    payload: NewsletterSignupSchema,
+):
     newsletter_signup(email=payload.email, location=payload.location)
+    response.set_cookie("newsletter_signup", "true", max_age=60 * 60 * 24 * 30)
     return {"message": "Thank you for being interested in Agora"}
